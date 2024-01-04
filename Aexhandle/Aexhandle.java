f0a34d1e7d88dc77f834eba5e2662f3fda8bdbc1
Tirtharaj <69608898+Tirtharaj-888@users.noreply.github.com>
2024-01-05 01:29:06 +0530

    feat: Aexhandle gets additions
    Code to demonstrate the "effect" of break statement in an exception handling construct.

--- Aexhandle/Aexhandle.java
+++ Aexhandle/Aexhandle.java
@@ -33,5 +33,58 @@ public class Aexhandle
             System.out.println("The integer quotient is: "+c);            
         }
         System.out.println("Fun Coding!");
+        
+        //new twist-1
+        try
+        {
+            int b = 5;
+            int a = 0;
+            int c = 0;
+            for(a =5; a>=0; a--){
+                 System.out.println("b ="+b+" a ="+a);
+                 if(a==0){
+                     break;
+                 }
+                 c = b/a;
+                 System.out.println("c ="+c);
+                 System.out.println(" ");
+            }
+            System.out.println("Division ends");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Program crash recovered!\n");
+        }
+        finally
+        {
+            System.out.println("Fun Coding!");
+        }
+        
+        //new twist-2 showing that if an exception occurs, break operation will be ignored and exception will be handled first
+        try
+        {
+            int b = 5;
+            int a = 0;
+            int c = 0;
+            for(a =5; a>=0; a--){
+                 System.out.println("b ="+b+" a ="+a);
+                 if(a==0){
+                     c=b/a;
+                     break;
+                 }
+                 c = b/a;
+                 System.out.println("c ="+c);
+                 System.out.println(" ");
+            }
+            System.out.println("Division ends");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Program crash recovered!\n");
+        }
+        finally
+        {
+            System.out.println("Fun Coding!");
+        }
     }
 }
--- ecommerce/NestLoops.java
+++ ecommerce/NestLoops.java
@@ -5,35 +5,38 @@ public class NestLoops
         int j=12;
         
         // Nested for-loop with inner break
-        for(int k = 1; k<=3; k++){
-            System.out.println("k ="+k);
-            for(int l = 1; l<=3; l++){
-                System.out.println(l);
-                if(l==2){
-                    break;
-                }
-                System.out.println(l+"th inner iteration done");
-            }
-            System.out.println(k+"th outer iteration done");
-        }
+        // for(int k = 1; k<=3; k++){
+            // System.out.println("k ="+k);
+            // for(int l = 1; l<=3; l++){
+                // System.out.println(l);
+                // if(l==2){
+                    // break;
+                // }
+                // System.out.println(l+"th inner iteration done");
+            // }
+            // System.out.println(k+"th outer iteration done");
+        // }
             
         // Nested do-while with inner continue
-        do{
-            j=j-1;
-            System.out.println("j="+j);
-            int i=10;
-            do
-            {
-                i=i-1;
-                if(i<=3)
-                {
-                continue;
-            }
-            System.out.println(i);
-        }while(i>0);
-        System.out.println(" ");
-        }while(j>0);
-        
+        // do{
+            // j=j-1;
+            // System.out.println("j="+j);
+            // int i=10;
+            // do
+            // {
+                // i=i-1;
+                // if(i<=3)
+                // {
+                // continue;
+            // }
+            // System.out.println(i);
+        // }while(i>0);
+        // System.out.println(" ");
+        // }while(j>0);
+        // if(a==0){
+                     // c = b/a;
+                     // break;
+                 // }
         
     
     }
